1da9e0e024e20b46299ae1eaefdf9f9ebe3a7dab
AnD <andersonventosilla0@gmail.com>
2021-09-07 18:13:03 -0500

    ude-poke

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>¿Quien es este pokemón?</h1>
+    
+    <PokemonPicture :pokemonId="534" :showPokemon="true" />
+        
+    <PokemonOptions />
+</template>
+
+<script>
+import PokemonOptions from '../components/PokemonOptions.vue'
+import PokemonPicture from '../components/PokemonPicture.vue'
+import getPokemonOptions from '../helpers/getPokemonOptions'
+
+console.log(getPokemonOptions());
+
+export default {
+    components: { PokemonOptions, PokemonPicture}
+}
+</script>
